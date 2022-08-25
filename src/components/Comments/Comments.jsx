1fe6c7b50c142bce5c49e32e54302b432367534b
erickmarquesc <erick.marquesc@hotmail.com>
2022-08-24 22:50:05 -0300

    feat: :sparkles: created component

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,36 @@
+import styles from './Comments.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from '../Avatar/Avatar';
+
+export function Comments() {
+  return (
+    <div className={styles.comment}>
+      <Avatar borderGreen={false}/>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Erick Marques</strong>
+              <time
+                title='21 de Agosto às 17:00h'
+                dateTime='2022-08-21 17:00:00'
+              >
+                Prublicado há 1h
+              </time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Dev, parabéns!! ✌️✌️</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
