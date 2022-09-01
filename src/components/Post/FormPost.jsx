a4cd4674da3fc1a1e677bb4c9a3313c9c6b71013
erickmarquesc <erick.marquesc@hotmail.com>
2022-09-01 13:18:17 -0300

    feat: :zap: add useState

--- src/components/Post/FormPost.jsx
+++ src/components/Post/FormPost.jsx
@@ -1,15 +1,31 @@
 import styles from './Post.module.css';
+import { Comments } from '../Comments/Comments';
+import { useState } from 'react';
 
 export function FormPost() {
+
+  const [comments, setComments] = useState([1, 2]);
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  };
+
   return (
-    <form className={styles.commentForm}>
-      <strong>Deixe eu feedback</strong>
-      <textarea
-        placeholder='Deixe um comentário'
-      />
-      <footer>
-        <button type="submit">Publicar</button>
-      </footer>
-    </form>
+    <>
+      <form
+        className={styles.commentForm}
+        onSubmit={handleCreateNewComment}
+      >
+        <strong>Deixe eu feedback</strong>
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <Comments commentsMap={comments}/>
+    </>
   );
 };
